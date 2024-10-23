61cf0c72b9ef01bec642ab9b02575d72cf824d17
Leila <leila.cheshmi@gmail.com>
2024-10-23 16:55:59 -0400

    add file

---
+++ inset_tables.py
@@ -0,0 +1,66 @@
+import sqlite3
+
+conn = sqlite3.connect('library_db.db')
+my_cursor = conn.cursor()
+
+# tables = books, authors, members, transactions, reviews
+
+create_author_table = """
+       CREATE TABLE IF NOT EXISTS authors(
+       author_id INTEGER PRIMARY KEY,
+       name TEXT NOT NULL,
+       country TEXT,
+       birth_year INTEGER
+       );"""
+
+create_book_table = """
+    CREATE TABLE IF NOT EXISTS books(
+    book_id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    author_id INTEGER,
+    published_year INEGER,
+    category TEXT,
+    available_copies INTEGER,
+    FOREIGN KEY(author_id) REFERENCES authors(author_id) 
+);"""
+
+create_members_table = """
+      CREATE TABLE IF NOT EXISTS members(
+      member_id INTEGER PRIMARY KEY,
+      name TEXT NOT NULL,
+      membership_data DATA,
+      email TEXT,
+      phone_number TEXT
+      );"""
+
+create_transaction_table = """
+      CREATE TABLE IF NOT EXISTS transactions(
+      transaction_id INTEGER PRIMARY KEY,
+      book_id INTEGER,
+      member_id INTEGER,
+      borow_date DATE,
+      return_date DATE,
+      FOREIGN KEY(book_id) REFERENCES books(book_id),
+      FOREIGN KEY (member_id) REFERENCES members(member_id)
+      );"""
+
+
+create_review_table = """
+    CREATE TABLE IF NOT EXISTS reviews(
+    review_id INTEGER PRIMARY KEY,
+    book_id INTEGER,
+    member_id INTEGER,
+    review_text TEXT,
+    rating INTEGER,
+    FOREIGN KEY(book_id) REFERENCES books(book_id),
+    FOREIGN KEY (member_id) REFERENCES members(member_id)
+    );"""
+
+my_cursor.execute(create_book_table)
+my_cursor.execute(create_author_table)
+my_cursor.execute(create_members_table)
+my_cursor.execute(create_transaction_table)
+my_cursor.execute(create_review_table)
+
+my_cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+print(my_cursor.fetchall())
